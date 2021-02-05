c0bbdb7cef09d3ee973f84d77d9b5d91a79d199c
argett <lilian.favre.garcia@efrei.net>
2021-02-05 20:34:40 +0100

    Creation of first scene + basic script + googleVR

---
+++ Rocket - Virtual Reality/Assets/Scripts/SelectObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour
+{
+    private float speed = 100;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            int layerMask = 0 << 10;
+            RaycastHit hit;
+            if (Physics.Raycast(player.transform.position + Camera.main.transform.forward, Camera.main.transform.forward, out hit))
+            {
+                hit.collider.gameObject.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV();
+                Debug.Log(hit.collider.gameObject.name);
+            }
+            else
+                Debug.Log("rien");
+
+        }
+    }
+}
---
+++ Rocket - Virtual Reality/Assets/Scripts/Walk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class Walk : MonoBehaviour
+{
+    public Transform vrCamera;
+    private CharacterController cc;
+    public float speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            cc.SimpleMove(forward * speed);
+        }
+    }
+}
