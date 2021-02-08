754df7ac61eb55ca4d75ef1d0d7f2ebb4b85eade
argett <lilian.favre.garcia@efrei.net>
2021-02-08 12:09:14 +0100

    Better tank & cap fit in the rocket creation

--- Rocket - Virtual Reality/Assets/Scripts/CreateRocket.cs
+++ Rocket - Virtual Reality/Assets/Scripts/CreateRocket.cs
@@ -24,30 +24,32 @@ public class CreateRocket : MonoBehaviour
 
     public void placeObject(GameObject part)
     {
-        size += part.transform.localScale.y;
+        float objSize = part.GetComponent<MeshRenderer>().bounds.size.y;
+        this.transform.position += new Vector3(0, objSize, 0);
         Transform parent = this.transform.parent;
+        size += objSize;
 
-        if (part.name == "Tank_1(Clone)")
-            Instantiate(carburant[0], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(0f, 0f, 0f), this.transform);
-        else if(part.name == "Tank_2(Clone)")
-            Instantiate(carburant[1], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(0f, 0f, 0f), this.transform);
-        else if (part.name == "Tank_3(Clone)")
-            Instantiate(carburant[2], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(0f, 0f, 0f), this.transform);
-
-        else if (part.name == "Coiffe_1(Clone)")
-            Instantiate(coiffe[0], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(180f, 0f, 0f), this.transform);
+        if (part.name == "Coiffe_1(Clone)")
+            Instantiate(coiffe[0], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(180f, 0f, 0f), this.transform);
         else if (part.name == "Coiffe_2(Clone)")
-            Instantiate(coiffe[1], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(180f, 0f, 0f), this.transform);
-        else if (part.name == "Coiffe_3(Clone)")
-            Instantiate(coiffe[2], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(180f, 0f, 0f), this.transform);
-        
+            Instantiate(coiffe[1], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(180f, 0f, 0f), this.transform);
+        else if (part.name == "Coiffe_3(Clone)") 
+            Instantiate(coiffe[2], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(180f, 0f, 0f), this.transform);
+
+        else if (part.name == "Tank_1(Clone)")
+            Instantiate(carburant[0], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(0f, 0f, 0f), this.transform);
+        else if(part.name == "Tank_2(Clone)")
+            Instantiate(carburant[1], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(0f, 0f, 0f), this.transform);
+        else if (part.name == "Tank_3(Clone)")
+            Instantiate(carburant[2], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(0f, 0f, 0f), this.transform);
+
+             
         else if (part.name == "Engine_1(Clone)")
-            Instantiate(moteur[0], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(0f, 0f, -90f), this.transform);
+            Instantiate(moteur[0], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(0f, 0f, 90f), this.transform);
         else if (part.name == "Engine_2(Clone)")
-            Instantiate(moteur[1], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(0f, 0f, -90f), this.transform);
+            Instantiate(moteur[1], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(0f, 0f, 90f), this.transform);
         else
-            Instantiate(moteur[2], new Vector3(parent.position.x, 0.1f, parent.position.z), Quaternion.Euler(0f, 0f, -90f), this.transform);
+            Instantiate(moteur[2], new Vector3(parent.position.x, objSize, parent.position.z), Quaternion.Euler(0f, 0f, 90f), this.transform);
         
-        this.transform.position += new Vector3(0, part.transform.localScale.y, 0);
     }
 }
