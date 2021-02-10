919743630e0cca9569ff9d5e996903b6f90724b1
adrien <adrien.chahinian@efrei.net>
2021-02-10 17:55:34 +0100

    Changed Destroy Flame Coroutine

--- Rocket - Virtual Reality/Assets/Scripts/Decolage.cs
+++ Rocket - Virtual Reality/Assets/Scripts/Decolage.cs
@@ -24,33 +24,27 @@ public class Decolage : MonoBehaviour
 
     public void decolage()
     {
+
         GameObject reactor = GameObject.FindGameObjectsWithTag("Reactor")[0];
         rocket = GameObject.FindGameObjectsWithTag("Rocket")[0];
         Instantiate(flame, reactor.transform.position, Quaternion.Euler(-90f, 0f, 0f), reactor.transform);
         rocket.GetComponent<Launch_Camera>().Creat_Cam();
-        while (carburant >0)
-        {        
-            //flame.transform.position += new Vector3(reactor.transform.position.x,reactor.transform.position.y -5, reactor.transform.position.z);
-            reactor.GetComponent<Rigidbody>().AddForce(transform.up * 1000);
-            carburant--;
-
-        }
-        carburant = 100;
         StartCoroutine(Destroy_Flame());
         
     }
 
     private IEnumerator Destroy_Flame()
     {
-        int s = 10;
-        GameObject reactor = GameObject.FindGameObjectsWithTag("Rocket")[0];
-        while (s >0)
+        GameObject reactor = GameObject.FindGameObjectsWithTag("Reactor")[0];
+        while (carburant > 0)
         {
-            // process
             Thread.Sleep(1000);
-            s--;
+            reactor.GetComponent<Rigidbody>().AddForce(transform.up * 1000);
+            carburant--;
             yield return null;
+
         }
+        carburant = 100;
         Destroy(GameObject.FindGameObjectsWithTag("Fire")[0]);
     }
 }
