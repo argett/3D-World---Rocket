720af554cb7802171bc904652eab45fa8dcc7b54
adrien <adrien.chahinian@efrei.net>
2021-02-10 20:31:35 +0100

    Added Max height and Speed

--- Rocket - Virtual Reality/Assets/Scripts/Decolage.cs
+++ Rocket - Virtual Reality/Assets/Scripts/Decolage.cs
@@ -3,16 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Threading;
 
 public class Decolage : MonoBehaviour
 {
-    public int carburant = 5;
-    private float dureecombustion;
-    public GameObject rocket, flame;
+    private int carburant = 15;
+    private float dureecombustion, Max, speed = 0, oldTime, oldAltitude;
+    public GameObject rocket, flame, theDisplay;
     // Start is called before the first frame update
     void Start()
     {
-
+        Max = rocket.transform.position.y;
+        theDisplay.GetComponent<Text>().text = "Height : " + rocket.transform.position.y.ToString();
         flame.SetActive(true);
 
     }
@@ -20,12 +22,32 @@ public class Decolage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Max!= rocket.transform.position.y)
+        {
+            speed = 
+            oldAltitude = rocket.transform.position.y;
+            oldTime = Time.time;
+        }
+        
+        if (rocket.transform.position.y >= Max)
+        {
+            theDisplay.GetComponent<Text>().text = "Height : " + rocket.transform.position.y.ToString() + " Speed : " + rocket.GetComponent<Rigidbody>().velocity.y;
+            Max = rocket.transform.position.y;
+        }
+        else
+        {
+            theDisplay.GetComponent<Text>().text = "Max Height : " + Max;
+        }
 
+        
     }
 
     public void decolage()
     {
-        dureecombustion = Time.time + carburant;
+        dureecombustion = Time.timeSinceLevelLoad + carburant;
+        print("carburant :" + carburant);
+        print("Time.time :"+Time.time);
+        print("dureecombustion :"+dureecombustion);
         GameObject reactor = GameObject.FindGameObjectsWithTag("Reactor")[0];
         rocket = GameObject.FindGameObjectsWithTag("Rocket")[0];
         Instantiate(flame, reactor.transform.position, Quaternion.Euler(-90f, 0f, 0f), reactor.transform);
@@ -39,7 +61,8 @@ public class Decolage : MonoBehaviour
         rocket = GameObject.FindGameObjectsWithTag("Rocket")[0];
         while (dureecombustion > Time.time)
         {
-            rocket.GetComponent<Rigidbody>().AddForce(transform.up * 10);
+            rocket.GetComponent<Rigidbody>().AddForce(transform.up * 3);
+            
             yield return null;
 
         }
@@ -47,8 +70,10 @@ public class Decolage : MonoBehaviour
         Destroy(GameObject.FindGameObjectsWithTag("Fire")[0]);
 
     }
-
+   
     
+
+
 
 }
 
