64de3205a06c623333baa7a9a6a0ff5c400d2ae9
argett <lilian.favre.garcia@efrei.net>
2021-02-15 18:24:41 +0100

    Added text containing the rocket part's data when pointer on it

--- Rocket - Virtual Reality/Assets/Scripts/SelectObject.cs
+++ Rocket - Virtual Reality/Assets/Scripts/SelectObject.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class SelectObject : MonoBehaviour
 {
     public GameObject player;
+    public GameObject caracteristic_rocket_part_prefab;
 
-    private float speed = 100;
+    private GameObject rocket_build;
     private GameObject halo = null;
-    public GameObject rocket_build;
+    private GameObject caracteristic_rocket_part;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] HALO = GameObject.FindGameObjectsWithTag("Halo");
-        Debug.Log("l = " + HALO.Length);
-        halo = HALO[0];
+        rocket_build = GameObject.FindGameObjectsWithTag("Rocket_pad")[0];
+        halo = GameObject.FindGameObjectsWithTag("Halo")[0];
         halo.SetActive(false);
-
-        GameObject[] ROCKET = GameObject.FindGameObjectsWithTag("Rocket_pad");
-        rocket_build = ROCKET[0];
+        caracteristic_rocket_part = null;
     }
 
     // Update is called once per frame
@@ -28,39 +26,96 @@ public class SelectObject : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(player.transform.position + Camera.main.transform.forward, Camera.main.transform.forward, out hit))
         {
-            //check if the object is a rocket part or not
-            if (hit.collider.gameObject.layer == 8)
+            
+            if (hit.collider.gameObject.layer == 8) //layer Rocket part
             {
+
+
                 halo.transform.position = hit.collider.gameObject.transform.position;
                 halo.SetActive(true);
 
                 if (Input.GetMouseButtonDown(0))
                 {
-                    rocket_build.GetComponent<CreateRocket>().placeObject(hit.collider.gameObject);
-                    // change the items
-                    GameObject[] button = GameObject.FindGameObjectsWithTag("GameController");
-                    button[0].GetComponent<PresentRocketParts>().pieceSelected();
+                    choseObject(hit.collider);
                 }
+                displayCaracteristics(hit.collider);
             }
-            else if(hit.collider.gameObject.tag == "Button")
+            else if (hit.collider.gameObject.layer == 9) //layer button, UI
             {
-                if (Input.GetMouseButtonDown(0))
+                if (hit.collider.gameObject.tag == "Button")
                 {
-                    switch (hit.collider.name)
+                    if (Input.GetMouseButtonDown(0))
                     {
-                        case "Add_stage":
-                            hit.collider.gameObject.GetComponent<Buttons>().Add_stage();
-                            break;
-                        case "Goto_lunch_pad":
-                            hit.collider.gameObject.GetComponent<Buttons>().Goto_lunch_pad();
-                            break;
+                        checkButtonType(hit.collider);
                     }
                 }
             }
             else
+            {
                 halo.SetActive(false);
+                if(caracteristic_rocket_part != null)
+                {
+                    Destroy(caracteristic_rocket_part);
+                    caracteristic_rocket_part = null;
+                }
+            }
         }
         else
+        {
             halo.SetActive(false);
+            if (caracteristic_rocket_part != null)
+            {
+                Destroy(caracteristic_rocket_part);
+                caracteristic_rocket_part = null;
+            }
+        }
+    }
+
+    private void displayCaracteristics(Collider obj)
+    {
+        if(caracteristic_rocket_part == null)
+        {
+            Vector3 parentPos = obj.gameObject.transform.position;
+            caracteristic_rocket_part = Instantiate(caracteristic_rocket_part_prefab, new Vector3(parentPos.x, parentPos.y, parentPos.z), Quaternion.identity);
+        }
+
+        Caracteristics rocketPart_info = obj.GetComponent<Caracteristics>();
+        int fuel = rocketPart_info.fuel;
+        int weight = rocketPart_info.weight;
+        int power = rocketPart_info.power;
+        int crew = rocketPart_info.crew;
+
+        string caract = "";
+        if (fuel != 0)
+            caract += "Total amount of fuel = " + fuel + "\n";
+        if (weight != 0)
+            caract += "Total weight of the part = " + weight + "\n";
+        if (power != 0)
+            caract += "Total power in Newton = " + power + "\n";
+        if (crew != 0)
+            caract += "Maximum crew = " + crew + "\n";
+
+        caracteristic_rocket_part.GetComponent<TextMesh>().text = caract;
+    }
+
+    private void choseObject(Collider obj)
+    {
+        rocket_build.GetComponent<CreateRocket>().placeObject(obj.gameObject);
+        // change the items
+        GameObject button = GameObject.FindGameObjectsWithTag("GameController")[0];
+        button.GetComponent<PresentRocketParts>().pieceSelected();
+    }
+
+    private void checkButtonType(Collider obj)
+    {
+        switch (obj.name)
+        {
+            case "Add_stage":
+                obj.gameObject.GetComponent<Buttons>().Add_stage();
+                break;
+            case "Goto_lunch_pad":
+                obj.gameObject.GetComponent<Buttons>().Goto_lunch_pad();
+                break;
+        }
     }
 }
